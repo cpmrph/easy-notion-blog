6adc3ca68315ce0798ff72e649bf2e8f182bca65
Koki Kubo <c11h17n6o8@gmail.com>
2022-04-29 15:18:51 +0900

    Modify footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,9 +6,14 @@ const Footer = () => (
   <footer className={styles.footer}>
     <div>
       <span>Powered by </span>
-      <ExtLink href="https://github.com/otoyo/easy-notion-blog">
-        easy-notion-blog
+      <ExtLink href="https://github.com/cpmrph/easy-notion-blog">
+        cpmrph/easy-notion-blog
       </ExtLink>
+      <span> (via </span>
+      <ExtLink href="https://github.com/otoyo/easy-notion-blog">
+        otoyo/easy-notion-blog
+      </ExtLink>
+      <span>)</span>
     </div>
   </footer>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import DocumentHead from '../components/document-head'
-import ExtLink from '../components/ext-link'
 import styles from '../styles/page.module.css'
 
 const RenderPage = () => (
@@ -7,15 +6,7 @@ const RenderPage = () => (
     <DocumentHead />
 
     <div>
-      <h2>Welcome!</h2>
-      <p>Your easy-notion-blog deployed successfully!</p>
-      <p>Have fun!</p>
-      <p>
-        easy-notion-blog powered by{' '}
-        <ExtLink href="https://github.com/otoyo/easy-notion-blog">
-          otoyo/easy-notion-blog
-        </ExtLink>
-      </p>
+      <h2>Anywhere but here.</h2>
     </div>
   </div>
 )
